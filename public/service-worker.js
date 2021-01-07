8e6c2dcd81ac895744adb39fa8f16d5e8c883775
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-06 21:32:18 -0500

    added files to be cached in the service-worker.js file

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "db.js",
+    "index.js"
+  ];
