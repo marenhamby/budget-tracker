cd4ed6ef0af62b828a51cbfd3c895709548b2af1
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-06 22:09:53 -0500

    corrected typos in link to service worker in index.html file and typos in files to cache on service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/style.css",
+    "/styles.css",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "db.js",
-    "index.js"
-  ];
+    "/db.js",
+    "/index.js"
+];
